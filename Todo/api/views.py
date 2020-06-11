7599558f98fbcbbb2166cc1bfb18453ddb8e637e
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-11 23:04:05 +0530

    Serializer and TaksList view

--- Todo/api/views.py
+++ Todo/api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import TaskSerializer
+from .models import Task
 # Create your views here.
 
 
@@ -15,3 +17,10 @@ def apiOverview(request):
         'Delete': '/task-delete/<str:pk>/'
     }
     return Response(api_urls)
+
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
