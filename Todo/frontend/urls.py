2a8f4ce68713d772192e2669f54e188ccf98b060
priyansh18 <48357563+priyansh18@users.noreply.github.com>
2020-06-12 21:29:37 +0530

    Views,Urls and Templates Added

--- Todo/frontend/urls.py
+++ Todo/frontend/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-# from . import views
+from . import views
 
 urlpatterns = [
-    # path('', views.apiOverview, name='api-overview'),
+    path('', views.list, name='list'),
 ]
